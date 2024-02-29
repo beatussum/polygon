1377027f8c1dd4d41b92b58a0431c4ed9509e61e
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2024-02-29 14:51:13 +0100

    `polygon::geo::Segment::distance_from_*`: add functions and their associated unit tests
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- src/geo/segment.rs
+++ src/geo/segment.rs
@@ -26,9 +26,50 @@ impl Segment {
         (dist - self.length()).abs() < EPSILON
     }
 
-    pub fn distance_from(&self, rhs: &Self) -> Unit
+    pub fn distance_from_point(&self, other: &Point) -> Unit
     {
-        Vector::from(self).orthogonal().dot(&rhs.into()).abs()
+        if self.contains(other) {
+            0.
+        } else {
+            let projection =
+                Vector::from((self.start, *other))
+                .dot(&(*self).into());
+
+            let oh =
+                Vector::from((Point::default(), self.start)) +
+                (projection / Vector::from(*self).squared_norm()) *
+                Vector::from(*self);
+
+            if self.contains(&oh.into()) {
+                (oh - (*other).into()).norm()
+            } else if projection < 0. {
+                self.start.distance_from(other)
+            } else {
+                self.stop.distance_from(other)
+            }
+        }
+    }
+
+    pub fn distance_from_segment(&self, other: &Self) -> Unit
+    {
+        fn dist(a: &Segment, b: &Segment) -> Unit
+        {
+            std::cmp::min_by(
+                a.distance_from_point(&b.start),
+                a.distance_from_point(&b.stop),
+                Unit::total_cmp
+            )
+        }
+
+        if self.is_secant_with(other) {
+            0.
+        } else {
+            std::cmp::min_by(
+                dist(self, other),
+                dist(other, self),
+                Unit::total_cmp
+            )
+        }
     }
 
     pub fn is_horizontal(&self) -> bool
@@ -43,11 +84,15 @@ impl Segment {
 
     pub fn is_secant_with(&self, rhs: &Self) -> bool
     {
-        let (a, b) = (*self).into();
-        let (c, d) = (*rhs).into();
+        if self == rhs {
+            true
+        } else {
+            let (a, b) = (*self).into();
+            let (c, d) = (*rhs).into();
 
-        (are_ccw(&a, &c, &d) != are_ccw(&b, &c, &d)) &&
-            (are_ccw(&a, &b, &c) != are_ccw(&a, &b, &d))
+            (are_ccw(&a, &c, &d) != are_ccw(&b, &c, &d)) &&
+                (are_ccw(&a, &b, &c) != are_ccw(&a, &b, &d))
+        }
     }
 
     pub fn length(&self) -> Unit { self.start.distance_from(&self.stop) }
@@ -78,6 +123,88 @@ mod tests
                 Point { x: 1., y: 1. }
             ).contains(&Point { x: 2., y: 2. })
         );
+    }
+
+    #[test]
+    fn test_distance_from_point_contained()
+    {
+        assert_eq!(
+            Segment::new(
+                Point { x: -1., y: -1. },
+                Point { x: 1., y: 1. }
+            ).distance_from_point(&Point::default()),
+            0.
+        );
+    }
+
+    #[test]
+    fn test_distance_from_point_in()
+    {
+        assert_eq!(
+            Segment::new(
+                Point { x: -1., y: -1. },
+                Point { x: 1., y: 1. }
+            ).distance_from_point(&Point { x: -1., y: 1. }),
+            2_f32.sqrt()
+        );
+    }
+
+    #[test]
+    fn test_distance_from_point_out()
+    {
+        assert_eq!(
+            Segment::new(
+                Point::default(),
+                Point { x: 1., y: 1. }
+            ).distance_from_point(&Point { x: -1., y: 0. }),
+            1.
+        );
+    }
+
+    #[test]
+    fn test_distance_from_segment_non_secant()
+    {
+        assert_eq!(
+            Segment::new(
+                Point::default(),
+                Point { x: 1., y: 1. }
+            ).distance_from_segment(
+                &Segment::new(
+                    Point { x: 4., y: 5. },
+                    Point { x: 3., y: 7. }
+                )
+            ),
+            5.
+        );
+    }
+
+    #[test]
+    fn test_distance_from_segment_secant()
+    {
+        assert_eq!(
+            Segment::new(
+                Point { x: -1., y: -1. },
+                Point { x: 1., y: 1. }
+            ).distance_from_segment(
+                &Segment::new(
+                    Point { x: -1., y: 1. },
+                    Point { x: 1., y: -1. }
+                )
+            ),
+            0.
+        );
+    }
+
+    #[test]
+    fn test_distance_from_segment_self()
+    {
+        let u =
+            Segment::new(
+                Point { x: -1., y: -1. },
+                Point { x: 1., y: 1. }
+            );
+
+        assert_eq!(u.distance_from_segment(&u), 0.);
     }
 
     #[test]
