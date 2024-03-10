01b110706b29d38e4fc0c6422a1627c7a995987a
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2024-03-10 18:23:32 +0100

    `polygon::parse_from_*`: add functions
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,108 @@
+use itertools::Itertools;
+
+use std::fs;
+use std::path::Path;
+
 pub mod geo;
+
+pub fn parse_from_string(str: &str) -> Vec<geo::Polygon>
+{
+    str
+        .lines()
+        .map(
+            |x| {
+                x
+                    .split_ascii_whitespace()
+                    .collect_tuple()
+                    .unwrap()
+            }
+        )
+        .map(
+            |(p, x, y)| {
+                (
+                    p.parse::<u8>().unwrap(),
+                    x.parse::<geo::Unit>().unwrap(),
+                    y.parse::<geo::Unit>().unwrap()
+                )
+            }
+        )
+        .group_by(|(p, _, _)| *p)
+        .into_iter()
+        .map(
+            |(_, group)| {
+                group.map(|(_, x, y)| (x, y).into()).collect::<Vec<_>>()
+            }
+        )
+        .map(|points| geo::Polygon { points })
+        .collect()
+}
+
+pub fn parse_from_file(path: &Path) -> Vec<geo::Polygon>
+{
+    parse_from_string(&fs::read_to_string(path).unwrap())
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    use geo::{Point, Polygon};
+    use indoc::indoc;
+
+    #[test]
+    fn test_parse_from_string()
+    {
+        let testing = indoc! {"
+            0 1 1
+            0 5 1
+            0 5 5
+            0 1 5
+            1 0 0
+            1 6 0
+            1 6 6
+            1 0 6
+            2 2 2
+            2 2 3
+            2 3 2
+            3 4 4
+            3 4 3
+            3 3 4
+        "};
+
+        let expected = vec![
+            Polygon {
+                points: vec! [
+                    Point { x: 1.0, y: 1.0 },
+                    Point { x: 5.0, y: 1.0 },
+                    Point { x: 5.0, y: 5.0 },
+                    Point { x: 1.0, y: 5.0 }
+                ]
+            },
+            Polygon {
+                points: vec! [
+                    Point { x: 0.0, y: 0.0 },
+                    Point { x: 6.0, y: 0.0 },
+                    Point { x: 6.0, y: 6.0 },
+                    Point { x: 0.0, y: 6.0 }
+                ]
+            },
+            Polygon {
+                points: vec! [
+                    Point { x: 2.0, y: 2.0 },
+                    Point { x: 2.0, y: 3.0 },
+                    Point { x: 3.0, y: 2.0 }
+                    ]
+            },
+            Polygon {
+                points: vec! [
+                    Point { x: 4.0, y: 4.0 },
+                    Point { x: 4.0, y: 3.0 },
+                    Point { x: 3.0, y: 4.0 }
+                ]
+            }
+        ];
+
+        assert_eq!(parse_from_string(testing), expected);
+    }
+}
