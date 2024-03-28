c052051441eb2637ac4951833999f57ff8223d83
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2024-03-28 15:44:11 +0100

    add command (`-s`) to print polygons in SVG
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use polygon::geo::SVG;
 use polygon::geo::generate_tree_from_polygons;
+
 use polygon::parse_from_file;
 
 use std::env::args;
@@ -7,19 +9,32 @@ use std::process::ExitCode;
 
 fn main() -> Result<(), ExitCode>
 {
-    if args().len() != 2 {
-        println!("USAGE: {} <input file>", args().next().unwrap());
+    if args().len() > 3 {
+        println!("USAGE: {} [-s] <input file>", args().nth(0).unwrap());
 
         return Err(ExitCode::FAILURE);
     }
 
-    let path = args().nth(1).unwrap();
+    let second = args().nth(1).unwrap();
+    let show = second == "-s";
+    let path = if show { args().nth(2).unwrap() } else { second };
     let path = Path::new(path.as_str());
     let nodes = parse_from_file(path);
-    let _root = generate_tree_from_polygons(&nodes);
 
-    for node in nodes {
-        print!("{} ", node.parent().unwrap().value().0);
+    if show {
+        println!("<svg>");
+
+        for node in nodes {
+            println!("\t{}", node.value().1.to_svg());
+        }
+
+        println!("</svg>");
+    } else {
+        let _root = generate_tree_from_polygons(&nodes);
+
+        for node in nodes {
+            print!("{} ", node.parent().unwrap().value().0);
+        }
     }
 
     println!();
